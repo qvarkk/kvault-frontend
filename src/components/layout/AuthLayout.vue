9592b32c58e245b0106a74e0b3973eb785c487a0
qvarkk <xumdes@gmail.com>
2026-05-09 15:49:03 +0500

    added form component

---
+++ src/components/layout/AuthLayout.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-scree flex items-center justify-center bg-background">
+    <div class="w-full max-w-sm px-4">
+      <slot />
+    </div>
+  </div>
+</template>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,57 +1,58 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-import { useAuthStore } from '@/stores/auth'
-import type { LoginPayload } from '@/types'
-import { Button } from '@/components/ui/button'
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { useAuthStore } from "@/stores/auth";
+import type { LoginPayload } from "@/types";
+import { Button } from "@/components/ui/button";
+import AuthLayout from "@/components/layout/AuthLayout.vue";
 
-const router = useRouter()
-const auth = useAuthStore()
+const router = useRouter();
+const auth = useAuthStore();
 
 const form = ref<LoginPayload>({
-  email: '',
-  password: '',
-})
+  email: "",
+  password: "",
+});
 
-const error = ref<string | null>(null)
-const loading = ref(false)
+const error = ref<string | null>(null);
+const loading = ref(false);
 
 async function handleSubmit() {
-  error.value = null
-  loading.value = true
+  error.value = null;
+  loading.value = true;
 
   try {
-    await auth.login(form.value)
-    router.push({ name: 'home' })
+    await auth.login(form.value);
+    router.push({ name: "home" });
   } catch (e: unknown) {
-    error.value = 'Invalid email or password'
+    error.value = "Invalid email or password";
   } finally {
-    loading.value = false
+    loading.value = false;
   }
 }
 </script>
 
 <template>
-  <div>
-    <form @submit.prevent="handleSubmit">
-      <input
-        v-model="form.email"
-        type="email"
-        placeholder="Email"
-        required
-      />
-      <input
-        v-model="form.password"
-        type="password"
-        placeholder="Password"
-        required
-      />
+  <AuthLayout>
+    <div>
+      
+    </div>
+    <!-- <div>
+      <form @submit.prevent="handleSubmit">
+        <input v-model="form.email" type="email" placeholder="Email" required />
+        <input
+          v-model="form.password"
+          type="password"
+          placeholder="Password"
+          required
+        />
 
-      <p v-if="error">{{ error }}</p>
+        <p v-if="error">{{ error }}</p>
 
-      <Button :disabled="loading">
-        {{ loading ? 'Logging in...' : 'Log in' }}
-      </Button>
-    </form>
-  </div>
+        <Button :disabled="loading">
+          {{ loading ? "Logging in..." : "Log in" }}
+        </Button>
+      </form>
+    </div> -->
+  </AuthLayout>
 </template>
